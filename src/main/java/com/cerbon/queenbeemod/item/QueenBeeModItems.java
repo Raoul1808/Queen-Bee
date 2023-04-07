79991b37901849c73460ff3864d64505f18d74b5
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-04-07 09:37:02 -0300

    Created Stinger item

--- src/main/java/com/cerbon/queenbeemod/item/QueenBeeModItems.java
+++ src/main/java/com/cerbon/queenbeemod/item/QueenBeeModItems.java
@@ -5,10 +5,13 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class QueenBeeModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, QueenBeeMod.MOD_ID);
 
+    public static final RegistryObject<Item> STINGER = ITEMS.register("stinger",
+            () -> new Item(new Item.Properties()));
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
     }

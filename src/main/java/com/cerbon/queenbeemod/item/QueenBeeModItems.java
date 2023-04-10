dd8dcb826610aa61c2f94846a12cc6bb986aef49
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-04-10 18:40:39 -0300

    Added Bee Antenna item

--- src/main/java/com/cerbon/queenbeemod/item/QueenBeeModItems.java
+++ src/main/java/com/cerbon/queenbeemod/item/QueenBeeModItems.java
@@ -1,6 +1,7 @@
 package com.cerbon.queenbeemod.item;
 
 import com.cerbon.queenbeemod.QueenBeeMod;
+import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,6 +13,9 @@ public class QueenBeeModItems {
 
     public static final RegistryObject<Item> STINGER = ITEMS.register("stinger",
             () -> new Item(new Item.Properties().food(QueenBeeModFoods.STINGER)));
+
+    public static final RegistryObject<Item> BEE_ANTENNA = ITEMS.register("bee_antenna",
+            () -> new ArmorItem(QueenBeeModArmorMaterials.BEE_ANTENNA, ArmorItem.Type.HELMET, new Item.Properties()));
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
     }

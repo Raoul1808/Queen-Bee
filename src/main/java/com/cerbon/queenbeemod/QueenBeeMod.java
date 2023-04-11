15ffd7297101bfd2b9281298e9cd60b598e50e54
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-04-10 21:47:21 -0300

    Erased some comments

--- src/main/java/com/cerbon/queenbeemod/QueenBeeMod.java
+++ src/main/java/com/cerbon/queenbeemod/QueenBeeMod.java
@@ -3,7 +3,6 @@ package com.cerbon.queenbeemod;
 import com.cerbon.queenbeemod.item.QueenBeeCreativeModeTabs;
 import com.cerbon.queenbeemod.item.QueenBeeModItems;
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.CreativeModeTabEvent;
@@ -16,13 +15,10 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(QueenBeeMod.MOD_ID)
 public class QueenBeeMod
 {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "queen_bee";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public QueenBeeMod()
@@ -30,38 +26,27 @@ public class QueenBeeMod
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         QueenBeeModItems.register(modEventBus);
-        // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
-
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-        // Register the item to a creative tab
-        modEventBus.addListener(this::addCreative);
+        modEventBus.addListener(this::commonSetup);
+        modEventBus.addListener(this::addCreativeTab);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event) {
-    }
+    private void commonSetup(final FMLCommonSetupEvent event) {}
 
-    private void addCreative(CreativeModeTabEvent.BuildContents event) {
+    private void addCreativeTab(CreativeModeTabEvent.BuildContents event) {
         if(event.getTab() == QueenBeeCreativeModeTabs.QUEEN_BEE_TAB){
             event.accept(QueenBeeModItems.STINGER);
         }
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
-    }
+    public void onServerStarting(ServerStartingEvent event) {}
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
     {
         @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
-        }
+        public static void onClientSetup(FMLClientSetupEvent event) {}
     }
 }

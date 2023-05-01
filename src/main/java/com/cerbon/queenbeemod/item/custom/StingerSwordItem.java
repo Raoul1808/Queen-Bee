b495549cf951527e0a20e4579e7cf1ce2ca98bcb
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-05-01 17:32:59 -0300

    Added tooltip for stinger sword

--- src/main/java/com/cerbon/queenbeemod/item/custom/StingerSwordItem.java
+++ src/main/java/com/cerbon/queenbeemod/item/custom/StingerSwordItem.java
@@ -1,12 +1,20 @@
 package com.cerbon.queenbeemod.item.custom;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class StingerSwordItem extends SwordItem {
     public StingerSwordItem(int pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties) {
@@ -48,5 +56,15 @@ public class StingerSwordItem extends SwordItem {
         pTarget.addEffect(new MobEffectInstance(MobEffects.POISON, 100, 0));
         pTarget.addEffect(new MobEffectInstance(MobEffects.CONFUSION, 300, 0));
         return super.hurtEnemy(pStack, pTarget, pAttacker);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        if (Screen.hasShiftDown()){
+            pTooltipComponents.add(Component.translatable("tooltips.stinger_sword.shift_down").withStyle(ChatFormatting.YELLOW));
+        }else {
+            pTooltipComponents.add(Component.translatable("tooltips.stinger_sword.shift_up").withStyle(ChatFormatting.YELLOW));
+        }
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
     }
 }

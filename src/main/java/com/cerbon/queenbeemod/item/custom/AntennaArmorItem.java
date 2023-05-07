46defd277e773e8deaa21db0b820474efdcd30d4
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-05-07 12:13:54 -0300

    Added tooltip for antenna

--- src/main/java/com/cerbon/queenbeemod/item/custom/AntennaArmorItem.java
+++ src/main/java/com/cerbon/queenbeemod/item/custom/AntennaArmorItem.java
@@ -1,20 +1,27 @@
 package com.cerbon.queenbeemod.item.custom;
 
 import com.cerbon.queenbeemod.client.item.renderer.AntennaArmorRenderer;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoItem;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInstanceCache;
 import software.bernie.geckolib.core.animation.*;
 import software.bernie.geckolib.core.object.PlayState;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 public class AntennaArmorItem extends ArmorItem implements GeoItem {
@@ -38,6 +45,16 @@ public class AntennaArmorItem extends ArmorItem implements GeoItem {
                 return this.renderer;
             }
         });
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        if (Screen.hasShiftDown()){
+            pTooltipComponents.add(Component.translatable("tooltips.antenna.shift_down").withStyle(ChatFormatting.YELLOW));
+        }else {
+            pTooltipComponents.add(Component.translatable("tooltips.antenna.shift_up").withStyle(ChatFormatting.YELLOW));
+        }
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
     }
 
     private PlayState predicate(AnimationState animationState){

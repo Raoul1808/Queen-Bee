be2d395f8938ad4bef241e1572caf54208387e49
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-05-10 14:45:26 -0300

    Added stinger sword right click ability

--- src/main/java/com/cerbon/queen_bee/item/custom/StingerSwordItem.java
+++ src/main/java/com/cerbon/queen_bee/item/custom/StingerSwordItem.java
@@ -1,11 +1,16 @@
 package com.cerbon.queen_bee.item.custom;
 
+import com.cerbon.queen_bee.item.QueenBeeModItems;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.animal.Bee;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
@@ -15,6 +20,8 @@ import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class StingerSwordItem extends SwordItem {
     public StingerSwordItem(int pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties) {
@@ -57,6 +64,40 @@ public class StingerSwordItem extends SwordItem {
         pTarget.addEffect(new MobEffectInstance(MobEffects.CONFUSION, 300, 0));
         return super.hurtEnemy(pStack, pTarget, pAttacker);
     }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+        if (pPlayer.isCrouching() && pPlayer.getItemBySlot(EquipmentSlot.HEAD).getItem() == QueenBeeModItems.ANTENNA.get()) {
+            Bee bee = EntityType.BEE.create(pLevel);
+            if (bee != null) {
+                bee.moveTo(pPlayer.getX(), pPlayer.getY() + 1, pPlayer.getZ());
+                bee.setInvulnerable(true);
+                bee.setNoAi(true);
+                pLevel.addFreshEntity(bee);
+            }
+            AreaEffectCloud areaEffectCloud = new AreaEffectCloud(pLevel, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ());
+            areaEffectCloud.setOwner(pPlayer);
+            areaEffectCloud.setDuration(200);
+            areaEffectCloud.setRadius(4.0F);
+            areaEffectCloud.setFixedColor(13458603);
+            areaEffectCloud.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 40, 1));
+            pLevel.addFreshEntity(areaEffectCloud);
+
+            pPlayer.swing(pUsedHand);
+            pPlayer.getCooldowns().addCooldown(this, 900);
+
+            // I think this is not the best way to do it. Not sure to be honest.
+            // TODO: Find a better way to schedule the bee death
+            new Timer().schedule(new TimerTask() { 
+                @Override
+                public void run() {
+                    bee.remove(Entity.RemovalReason.KILLED);
+                }
+            }, 11700);
+        }
+        return super.use(pLevel, pPlayer, pUsedHand);
+    }
+
 
     @Override
     public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {

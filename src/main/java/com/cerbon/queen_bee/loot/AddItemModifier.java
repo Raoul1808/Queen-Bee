30462b2c655e377f5b4410dcc6f93477a3bf53ca
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-05-29 18:24:13 -0300

    Add stinger item drop to any entity instanceof bee

--- src/main/java/com/cerbon/queen_bee/loot/AddItemModifier.java
+++ src/main/java/com/cerbon/queen_bee/loot/AddItemModifier.java
@@ -4,9 +4,11 @@ import com.google.common.base.Suppliers;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.world.entity.animal.Bee;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.common.loot.LootModifier;
@@ -28,12 +30,21 @@ public class AddItemModifier extends LootModifier {
 
     @Override
     protected @NotNull ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
-        generatedLoot.add(new ItemStack(item));
+        if (checkIfInjectLoot(context)){
+            generatedLoot.add(new ItemStack(item));
+        }
         return generatedLoot;
     }
 
     @Override
     public Codec<? extends IGlobalLootModifier> codec() {
         return CODEC.get();
+    }
+
+    public static boolean checkIfInjectLoot(LootContext context) {
+        if (context.hasParam(LootContextParams.THIS_ENTITY)) {
+            return context.getParam(LootContextParams.THIS_ENTITY) instanceof Bee;
+        }
+        return false;
     }
 }
